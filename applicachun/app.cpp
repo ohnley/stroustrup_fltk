9fa453a680e6394798846181c4371d402b7770d2
Ohnley <28885446+ohnley@users.noreply.github.com>
2021-05-29 15:57:17 -0700

    cleaned main function

--- applicachun/app.cpp
+++ applicachun/app.cpp
@@ -174,68 +174,10 @@ void ch13ex04(){
 }
 
 int main(){
-
     using namespace Graph_lib; //our graphics facilities are in Graph_lib (defined in Graph.h)
-
-    // Point top_left{600, 300};    //will be top left corner of window
-
-    // Simple_window win {top_left, 600, 600, "Canvas"};
-    // ch13ex02();
-    ch13ex04();
-    ch_13drill();
-    // ex04(win);
-    // ex06(win);
-    // ex09(win);
-    // ch13_grid(win);
-    // win.wait_for_button();
-
-
-    // Axis xa {Axis::x, Point{20,350},280,10,"x-axeees"};
-    // xa.set_color(Color::dark_magenta);
-    // win.attach(xa);
-    // Axis ya {Axis::y, Point{20,350}, 280, 10, "y axiiss"};
-    // win.attach(ya);
-    // win.set_label("20,000 LBS OF BANANAS");
-    // Function sine {sin, 0, 100, Point{20,150}, 1000, 50, 50};
-    // sine.set_color(Color::blue);
-    // win.attach(sine);
-
-    // poly.add(Point{300,200}); // three points make a triangle
-    // poly.add(Point{350,100});
-    // poly.add(Point{400,200});
-    // poly.set_color(Color::red);
-    // poly.set_style(Line_style::dash);
-    // win.attach(poly);
-
-    // Graph_lib::Rectangle r {Point{200,200}, 300, 30}; // top left corner, width, height
-    // win.attach(r);
-
-    // Closed_polyline poly_rect;
-    // poly_rect.add(Point{100,50});
-    // poly_rect.add(Point{200,50});
-    // poly_rect.add(Point{200,100});
-    // poly_rect.add(Point{100,100});
-    // poly_rect.add(Point{50,75});
-    // win.attach(poly_rect);
-
-    // r.set_fill_color(Color::yellow); // color the inside of the rectangle
-    // poly.set_style(Line_style(Line_style::dash,4));
-    // poly_rect.set_style(Line_style(Line_style::dash,2));
-    // poly_rect.set_fill_color(Color::green);
-
-
-    // Text t {Point{150,150}, "Hello, graphical world!"};
-    // win.attach(t);
-    // win.set_label("Canvas #8");
-
-    // win.wait_for_button();  //give control to the display engine
+    
+    
+    
     return 0;
 }
 
-// void printer(const vector<int> list){
-//     for (int i : list){
-//         std::cout << i << " ";
-//     }
-    
-//     std::cout << '\n';
-// }
